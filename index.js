4bab7023838ab2caf691962de33df213a82cb2ab
beProsto <lavionperavion2@gmail.com>
2020-04-20 22:05:05 +0200

    Added ground and textures!

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ let polyfill = new WebXRPolyfill();
 // XR globals.
 let xrButton = document.getElementById("xr-button");
 let xrSession = null;
-let xrRefSpace = null		
-// WebGL scene globals.
-let gl = null;
+let xrRefSpace = null;
 
-let glObjs = {vertexbuffer: null, vertexarray: null, program: null};
+let shader = null;
+
+let controllerVertexBuffer = null;
+let controllerTexture = null;
+let groundVertexBuffer = null;
+let groundTexture = null;
+let controllers = [];
 
 // Creates html canvas element and returns it's WebGL 2 context.
 function createWebGLContext(glAttribs) {
@@ -60,73 +64,109 @@ function onSessionStarted(session) {
 	});
 
 	/* Creating the vertex buffer and setting up the vertex layout */
-	glObjs.vertexarray = gl.createVertexArray();
-	gl.bindVertexArray(glObjs.vertexarray);
-
 	const vertices = [
-		-0.5, -0.5, -1.0,
-			1.0, 0.0, 0.0, 1.0,
-		0.0, 0.5, -1.0,
-			0.0, 1.0, 0.0, 1.0,
-		0.5, -0.5, -1.0,
-			0.0, 0.0, 1.0, 1.0
+		-0.15, 0.10, 0.10,
+			0.0, 0.0,
+		0.0, -0.10, -0.10,
+			0.5, 1.0,
+		0.15, 0.10, 0.10,
+			1.0, 0.0
 	];
-	glObjs.vertexbuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, glObjs.vertexbuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	
-	gl.bindVertexArray(glObjs.vertexarray);
+	const vertices2 = [
+		-1.0, -1.0, -1.0,
+			0.0, 0.0,
+		-1.0, -1.0, 1.0,
+			0.0, 1.0,
+		1.0, -1.0, 1.0,
+			1.0, 1.0,
+		
+		1.0, -1.0, 1.0,
+			1.0, 1.0,
+		1.0, -1.0, -1.0,
+			1.0, 0.0,
+		-1.0, -1.0, -1.0,
+			0.0, 0.0,
+	];
 
-	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 7*4, 0);
-	gl.enableVertexAttribArray(0);
-	gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 7*4, 3*4);
-	gl.enableVertexAttribArray(1);
-	
-	gl.bindVertexArray(null); /* we have to unbind the vertex array, because WebXR does some vertex layout setting on it's own later, and we don't want it to influence our vertex array */
-	
-	/* Creating and compiling shaders and combining them into a program */
+	controllerVertexBuffer = new ezgl.VertexBuffer();
+	controllerVertexBuffer.vertexLayout([3, 2]);
+	controllerVertexBuffer.vertexData(vertices);
+
+	groundVertexBuffer = new ezgl.VertexBuffer();
+	groundVertexBuffer.vertexLayout([3, 2]);
+	groundVertexBuffer.vertexData(vertices2);
+
 	const vertCode = "#version 300 es\n\
 	precision mediump float;\n\
 	\n\
 	layout(location = 0) in vec3 a_Position;\n\
-	layout(location = 1) in vec4 a_Color;\n\
+	layout(location = 1) in vec2 a_TexCoord;\n\
 	\n\
 	uniform mat4 u_Projection;\n\
 	uniform mat4 u_View;\n\
+	uniform mat4 u_Model;\n\
 	\n\
-	out vec4 v_Color;\n\
+	out vec2 v_TexCoord;\n\
 	\n\
 	void main() {\n\
-		gl_Position = u_Projection * u_View * vec4(a_Position, 1.0);\n\
-		v_Color = a_Color;\n\
+		gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 1.0);\n\
+		v_TexCoord = a_TexCoord;\n\
+		v_TexCoord.y = 1.0 - v_TexCoord.y;\n\
 	}";
-	let vertShader = gl.createShader(gl.VERTEX_SHADER);
-	gl.shaderSource(vertShader, vertCode);
-	gl.compileShader(vertShader);
-	
 	const fragCode = "#version 300 es\n\
 	precision mediump float;\n\
 	\n\
 	out vec4 o_Color;\n\
 	\n\
-	in vec4 v_Color;\n\
+	in vec2 v_TexCoord;\n\
+	\n\
+	uniform sampler2D u_Texture;\n\
 	\n\
 	void main() {\n\
-		o_Color = v_Color;\n\
+		o_Color = texture(u_Texture, v_TexCoord);\n\
 	}";
-	let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fragShader, fragCode);
-	gl.compileShader(fragShader);
-	
-	glObjs.program = gl.createProgram();
-	gl.attachShader(glObjs.program, vertShader);
-	gl.attachShader(glObjs.program, fragShader);
-	gl.linkProgram(glObjs.program);
-	gl.useProgram(glObjs.program);
 
+	let vS = new ezgl.SubShader(gl.VERTEX_SHADER, vertCode);
+	let fS = new ezgl.SubShader(gl.FRAGMENT_SHADER, fragCode);
+
+	shader = new ezgl.Shader();
+	shader.join(vS);
+	shader.join(fS);
+	shader.link();
+
+	fS.free();
+	vS.free();
+
+	controllerTexture = new ezgl.Texture();
+	controllerTexture.fromFile("/tex.png");
+	controllerTexture.bind();
+
+	groundTexture = new ezgl.Texture();
+	groundTexture.fromFile("/tex2.png");
+	groundTexture.bind();
+
+	gl.enable(gl.DEPTH_TEST);
 }
 function onSessionEnded(event) {
 	xrSession = null;
+	groundTexture.free();
+	controllerTexture.free();
+	shader.free();
+	controllerVertexBuffer.free();
+	groundVertexBuffer.free();
+}
+
+function onControllerUpdate(session, frame) {
+	let i = 0;
+	for(let inputSource of session.inputSources) {
+		if(inputSource.gripSpace) {
+			let gripPose = frame.getPose(inputSource.gripSpace, xrRefSpace);
+			if(gripPose) {
+				controllers[i] = {pose: gripPose, hand: inputSource.handedness};
+			}
+		}
+		i++;
+	}
 }
 
 function onXRFrame(t, frame) {
@@ -138,6 +178,8 @@ function onXRFrame(t, frame) {
 	if(pose) {
 		let glLayer = session.renderState.baseLayer;
 		
+		onControllerUpdate(session, frame);
+
 		gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
 		
 		gl.clearColor(0.4, 0.7, 0.9, 1.0);
@@ -148,20 +190,23 @@ function onXRFrame(t, frame) {
 			let viewport = glLayer.getViewport(view);
 			gl.viewport(viewport.x, viewport.y, viewport.width, viewport.height);
 
-			gl.useProgram(glObjs.program);
-			gl.bindVertexArray(glObjs.vertexarray);
-			gl.bindBuffer(gl.ARRAY_BUFFER, glObjs.vertexbuffer);
+			shader.bind();
 
-			let pLoc = gl.getUniformLocation(glObjs.program, "u_Projection");
-			gl.uniformMatrix4fv(pLoc, false, view.projectionMatrix);
-			let vLoc = gl.getUniformLocation(glObjs.program, "u_View");
-			gl.uniformMatrix4fv(vLoc, false, view.transform.inverse.matrix);
+			shader.set1i("u_Texture", 0);
+			shader.set4x4f("u_Projection", view.projectionMatrix);
+			shader.set4x4f("u_View", view.transform.inverse.matrix);
+			
+			controllerTexture.bind();
+			shader.set4x4f("u_Model", controllers[0].pose.transform.matrix);
+			controllerVertexBuffer.draw();
+			shader.set4x4f("u_Model", controllers[1].pose.transform.matrix);
+			controllerVertexBuffer.draw();
 
-			gl.drawArrays(gl.TRIANGLES, 0, 3);
+			groundTexture.bind()
+			shader.set4x4f("u_Model", glMatrix.mat4.create());
+			groundVertexBuffer.draw();
 
-			gl.bindVertexArray(null); /* the same reason as previously */
-
-			//renderScene(view.projectionMatrix, view.transform);
+			shader.unbind();
 		}
 	}
 }
