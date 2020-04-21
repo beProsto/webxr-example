69ff66735b936eed2ebba5f9ca0e5aedbbe78076
beProsto <lavionperavion2@gmail.com>
2020-04-21 20:17:30 +0200

    Model loading from files, Input being handled, Everything works nicely

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let xrRefSpace = null;
 
 let shader = null;
 let controllerVertexBuffer = null;
+let controllerVertexBuffer2 = null;
 let controllerTexture = null;
 let groundVertexBuffer = null;
 let groundTexture = null;
@@ -47,10 +48,9 @@ function onButtonClicked() {
 	}
 }
 
-const objstr = 
-"v 0.0 0.0 0.0\nv 0.0 1.0 0.0\nv 1.0 1.0 0.0\nvt 0.0 0.0\nvt 0.5 1.0\nvt 1.0 0.0\nvn 0.0 0.0 1.0\nf 1/1/1 2/2/1 3/3/1";
+const objstr = "v 0.0 0.0 0.0\nv 0.0 1.0 0.0\nv 1.0 1.0 0.0\nvt 0.0 0.0\nvt 0.5 1.0\nvt 1.0 0.0\nvn 0.0 0.0 1.0\nf 1/1/1 2/2/1 3/3/1";
 const vertices = ezobj.load(objstr);
-console.log(vertices);
+
 function onSessionStarted(session) {
 	xrSession = session;
 	session.addEventListener("end", onSessionEnded);
@@ -66,27 +66,42 @@ function onSessionStarted(session) {
 		session.requestAnimationFrame(onXRFrame);
 	});
 
-	/* Creating the vertex buffer and setting up the vertex layout */
-	
-	const vertices2 = [
-		-1.0, -1.0, -1.0,
-			0.0, 0.0,
-		-1.0, -1.0, 1.0,
-			0.0, 1.0,
-		1.0, -1.0, 1.0,
-			1.0, 1.0,
-		
-		1.0, -1.0, 1.0,
-			1.0, 1.0,
-		1.0, -1.0, -1.0,
-			1.0, 0.0,
-		-1.0, -1.0, -1.0,
-			0.0, 0.0,
-	];
-
+	/* Creating the vertex buffers and setting up the vertex layouts */
 	controllerVertexBuffer = new ezgl.VertexBuffer();
 	controllerVertexBuffer.vertexLayout([3, 2, 3]);
 	controllerVertexBuffer.vertexData(vertices);
+	fetch("/hand_open.obj").then(response => {
+		response.text().then(text => {
+			const verticesLoaded = ezobj.load(text);
+			controllerVertexBuffer.vertexData(verticesLoaded);
+		});
+	});	
+	controllerVertexBuffer2 = new ezgl.VertexBuffer();
+	controllerVertexBuffer2.vertexLayout([3, 2, 3]);
+	controllerVertexBuffer2.vertexData(vertices);
+	fetch("/hand_closed.obj").then(response => {
+		response.text().then(text => {
+			const verticesLoaded = ezobj.load(text);
+			controllerVertexBuffer2.vertexData(verticesLoaded);
+		});
+	});
+
+	const vertices2 = [
+		-1.0, 0.0, -1.0,
+			0.0, 0.0,
+		-1.0, 0.0, 1.0,
+			0.0, 1.0,
+		1.0, 0.0, 1.0,
+			1.0, 1.0,
+		
+		1.0, 0.0, 1.0,
+			1.0, 1.0,
+		1.0, 0.0, -1.0,
+			1.0, 0.0,
+		-1.0, 0.0, -1.0,
+			0.0, 0.0,
+	];
+
 
 	groundVertexBuffer = new ezgl.VertexBuffer();
 	groundVertexBuffer.vertexLayout([3, 2]);
@@ -177,6 +192,16 @@ function onXRFrame(t, frame) {
 		
 		onControllerUpdate(session, frame);
 
+		let c0grip = false;
+		let c1grip = false;
+
+		if(controllers[0].gamepad.buttons[1].value > 0.5) {
+			c0grip = true;
+		}
+		if(controllers[1].gamepad.buttons[1].value > 0.5) {
+			c1grip = true;
+		}
+
 		gl.bindFramebuffer(gl.FRAMEBUFFER, glLayer.framebuffer);
 		
 		gl.clearColor(0.4, 0.7, 0.9, 1.0);
@@ -194,13 +219,41 @@ function onXRFrame(t, frame) {
 			shader.set4x4f("u_View", view.transform.inverse.matrix);
 			
 			controllerTexture.bind();
-			shader.set4x4f("u_Model", controllers[0].pose.transform.matrix);
-			controllerVertexBuffer.draw();
-			shader.set4x4f("u_Model", controllers[1].pose.transform.matrix);
-			controllerVertexBuffer.draw();
+
+			let model = glMatrix.mat4.create();
+			let mat = glMatrix.mat4.create();
+			glMatrix.mat4.rotateX(mat, mat, -Math.PI / 2.0);
+			glMatrix.mat4.rotateY(mat, mat, -Math.PI / 2.0);
+			glMatrix.mat4.rotateZ(mat, mat, -Math.PI / 8.0);
+			glMatrix.mat4.scale(mat, mat, [0.3, 0.3, -0.3]);
+			glMatrix.mat4.multiply(model, controllers[0].pose.transform.matrix, mat);
+			shader.set4x4f("u_Model", model);
+			if(!c0grip) {
+				controllerVertexBuffer.draw();
+			}
+			else {
+				controllerVertexBuffer2.draw();
+			}
+
+			model = glMatrix.mat4.create();
+			mat = glMatrix.mat4.create();
+			glMatrix.mat4.rotateX(mat, mat, -Math.PI / 2.0);
+			glMatrix.mat4.rotateY(mat, mat, -Math.PI / 2.0);
+			glMatrix.mat4.rotateZ(mat, mat, -Math.PI / 8.0);
+			glMatrix.mat4.scale(mat, mat, [0.3, 0.3, 0.3]);
+			glMatrix.mat4.multiply(model, controllers[1].pose.transform.matrix, mat);
+			shader.set4x4f("u_Model", model);
+			if(!c1grip) {
+				controllerVertexBuffer.draw();
+			}
+			else {
+				controllerVertexBuffer2.draw();
+			}
 
 			groundTexture.bind()
-			shader.set4x4f("u_Model", glMatrix.mat4.create());
+			model = glMatrix.mat4.create();
+			glMatrix.mat4.translate(model, model, [0.0, -1.6, 0.0]);
+			shader.set4x4f("u_Model", model);
 			groundVertexBuffer.draw();
 
 			shader.unbind();
